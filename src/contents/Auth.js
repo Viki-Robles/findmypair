9711f3a6c6dc709e7688e067913d02c8a76a487d
Vicky Vasilopoulou <vickyvasilopoulou@Vickys-MacBook.local>
2021-02-16 10:41:12 +0000

    feat: firebase setup - signup form build

---
+++ src/contents/Auth.js
@@ -0,0 +1,39 @@
+import React, { useContext, useState, useEffect } from 'react';
+import { auth } from '../firebase';
+
+const AuthContext = React.createContext()
+
+export function useAuth() {
+    return useContext(AuthContext)
+}
+
+export function AuthProvider({ children }) {
+    const [currentUser, setCurrentUser] = useState();
+
+    //if i want to connect to my own server just change those two functions below
+    function signup(email, password) {
+        return auth.createUserWithEmailAndPassword(email, password)
+    }
+
+    useEffect(() => {
+        //function that firebase notifies you if a user is set
+        const unsubsrcibe = auth.onAuthStateChanged(user => {
+            setCurrentUser(user)
+        })
+        return unsubsrcibe
+    }, [])
+
+
+    const value = {
+        currentUser, 
+        signup,
+        
+    }
+
+
+    return (
+        <AuthContext.Provider value={value}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
